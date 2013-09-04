64f6f4e72fb023cec65b95663839590eb892a50b
Andy <andy@i2you.ru>
2013-09-04 21:56:02 +0400

    add syslog and uncaught exception wrapper

--- example/simple.js
+++ example/simple.js
@@ -4,8 +4,10 @@
 
 var LoggerFactory = require('../index');
 
-LoggerFactory.configure(__dirname + '/' + 'config.yaml', {wrapConsole: true, rootDir: __dirname, modules: {adapters: '../lib/adapters', formatProcessors: '../lib/format.processors'}}).env().init();
+LoggerFactory.configure(__dirname + '/' + 'config.yaml', {wrapConsole: true, wrapUncaught: true, rootDir: __dirname, modules: {adapters: '../lib/adapters', formatProcessors: '../lib/format.processors'}}).env().init();
 
 var logger = LoggerFactory.getLogger('ssss');
+
+throw new Error('Uncaught');
 
 console.info('info', new Error('some error'), new Error('some error2'), {data: {attr1: true}});
