8bffece34fd79a0fd9e04641e550e163c1ca4b7b
Андрей Сумской <sumskoy@realweb.ru>
2013-09-06 11:11:27 +0400

    log self errors

--- example/simple.js
+++ example/simple.js
@@ -6,8 +6,6 @@ var LoggerFactory = require('../index');
 
 LoggerFactory.configure(__dirname + '/' + 'config.yaml', {wrapConsole: true, wrapUncaught: true, rootDir: __dirname, modules: {adapters: '../lib/adapters', formatProcessors: '../lib/format.processors'}}).env().init();
 
-var logger = LoggerFactory.getLogger('ssss');
+var logger = LoggerFactory.getLogger('test.mongo');
 
-throw new Error('Uncaught');
-
-console.info('info', new Error('some error'), new Error('some error2'), {data: {attr1: true}});
+logger.error('info', new Error('some error'), new Error('some error2'), {data: {attr1: true}});
